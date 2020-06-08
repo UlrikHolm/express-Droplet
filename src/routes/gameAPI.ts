5e14d6026f39307043c6797eed8253d55e26c631
Ulrik Holm <ulrikholmuh@gmail.com>
2020-06-08 17:04:03 +0200

    fixed wrong files :P

---
+++ src/routes/gameAPI.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import gameFacade from "../facades/gameFacade";
+const router = express.Router();
+import { ApiError } from "../errors/apiError"
+
+//import * as mongo from "mongodb"
+import setup from "../config/setupDB"
+import UserFacade from '../facades/userFacadeWithDB';
+
+(async function setupDB() {
+  const client = await setup()
+  gameFacade.setDatabase(client)
+})()
+
+router.post('/nearbyplayers', async function (req, res, next) {
+    try {
+        const result = await gameFacade.nearbyPlayers(
+          req.body.userName, 
+          req.body.password, 
+          req.body.lon, 
+          req.body.lat,
+          req.body.distance
+          )
+        res.json(result)
+    } catch (err) {
+        next(err)
+    }
+
+})
+router.post('/getPostIfReached', async function (req, res, next) {
+    try{
+      const result = await gameFacade.getPostIfReached(
+        req.body.postId,
+        req.body.lat,
+        req.body.lon
+      )
+      res.json(result)
+    } catch(err){
+      next(err)
+    }
+
+    //throw new Error("Not yet implemented")
+})
+
+module.exports = router;
